3c542cb093a6ad783b1b04b6f237a2fd73c4c126
pokemonbyt <ngoclonggm@gmail.com>
2023-04-29 14:13:03 +0800

    Create and commit code

---
+++ src/enums/common.js
@@ -0,0 +1,15 @@
+export const userRolesEnums = [
+  { id: 99, name: 'Admin' },
+  { id: 5, name: 'THVL' },
+  { id: 4, name: 'HLV' },
+  { id: 3, name: 'TT' },
+  { id: 2, name: 'NV' },
+  { id: 1, name: 'KH' },
+];
+export const userRolesNotAdminEnums = [
+  { id: 5, name: 'THVL' },
+  { id: 4, name: 'HLV' },
+  { id: 3, name: 'TT' },
+  { id: 2, name: 'NV' },
+  { id: 1, name: 'KH' },
+];
